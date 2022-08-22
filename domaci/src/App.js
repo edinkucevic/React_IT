928e22cd951515857e82db82712da2415c98e5e2
Edin Kucevic <edinkucevic99@gmail.com>
2022-08-22 17:50:27 +0200

    domaci

---
+++ domaci/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import CryptoForm from "./components/Crypto";
+
+function App() {
+  return (
+    <div className="card-container">
+      <CryptoForm />
+    </div>
+  );
+}
+
+export default App;
